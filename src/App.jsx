4238f9a306b403ff499763621c143bf3e57d59fa
mariaquintero6 <aleja.qc12@gmail.com>
2024-11-07 06:35:26 -0500

    cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import Cards from '../src/Componentes/cards'
+import './App.css';
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [characters, setCharacters] = useState([]);
+
+  const setlist = async () => {
+    try {
+      const response = await fetch('https://rickandmortyapi.com/api/character');
+      const personajes = await response.json();
+      setCharacters(personajes.results); // Suponemos que contiene la lista de personajes
+      console.log('Los personajes son: ', personajes.results);
+    } catch (error) {
+      console.error('Error:', error.message);
+    }
+  };
+
+  useEffect(() => {
+    setlist(); // Llama a `setlist` cuando el componente se monta
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='container-fluid'>
+      <div className="row">
+        <h1 className='Title'> The Rick and Morty API</h1>
+
+        <div className='List  d-flex  flex-wrap '>
+          {characters.map((character) => (
+            <div className='Personajes' key={character.id}>
+              <Cards
+                Foto={character.image}
+                Nombre={character.name}
+                Estado={character.status}
+                Especie={character.species}
+                Genero={character.gender}
+              />
+            </div>
+          ))}
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
